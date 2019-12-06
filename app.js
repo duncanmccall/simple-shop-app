59fac184e3a8011e7651c5549e9f4ca6686d2822
Duncan McCall <mccall.duncan@gmail.com>
2019-12-05 22:00:33 -0500

    Added generic error handling for all undefined routes and all other errors

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const productsRouter = require('./routes/products');
 const ordersRouter = require('./routes/orders');
 
+app.use(morgan('dev'));
+
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
+
+/*
+    Error handling
+*/
+
+// handle all undefined routes and return a 404
+app.use((req, res, next) => {
+    const error = new Error('404 Error: Resource not found');
+    error.status = 404;
+    next(error);
+});
+
+// handle all errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 
 module.exports = app;
