3f3b1c70f16f2c6deb0d9e1b700fb79071497cbc
Duncan McCall <mccall.duncan@gmail.com>
2019-12-05 21:14:05 -0500

    Basic routes for products completed

--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,37 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handing GET requests to /products'
+        message: 'Handling GET requests to /products'
     });
 });
 
 router.post('/', (req, res, next) => {
     res.status(200).json({
-        message: 'Handing POST requests to /products'
+        message: 'Handling POST requests to /products'
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    res.status(200).json({
+        message: 'Handling GET requests to /products/productId',
+        id: id
+    });
+});
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    res.status(200).json({
+        message: 'Handling PATCH requests to /products/productId',
+        id: id
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    res.status(200).json({
+        message: 'Handling DELETE requests to /products/productId',
+        id: id
     });
 });
 
