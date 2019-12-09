baf5e63bdc26a151402d7d9922ee763953596933
Duncan McCall <mccall.duncan@gmail.com>
2019-12-09 11:15:01 -0500

    Updated product database operations

--- routes/products.js
+++ routes/products.js
@@ -5,9 +5,20 @@ const Product = require('../models/Product');
 
 // GET to /products
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    });
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs);
+            res.status(200).json({
+                message: 'Handling GET requests to /products',
+                docs
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error });
+        });
+
 });
 
 // POST to /products
@@ -24,30 +35,38 @@ router.post('/', (req, res, next) => {
     product.save()
         .then((result) => {
             console.log(result);
+            res.status(201).json({
+                message: 'Handling POST requests to /products',
+                createdProduct: result
+            });
         }).catch((error) => {
             console.log(error);
+            res.status(500).json({ error });
         });
-
-    // send response to client
-    res.status(201).json({
-        message: 'Handling POST requests to /products',
-        createdProduct: product
-    });
 
 });
 
 // GET to /products/:productId
 router.get('/:productId', (req, res, next) => {
+    
+    // get product id from params
     const id = req.params.productId;
 
+    // find product by id
     Product.findById(id)
         .exec()
         .then(doc => {
             console.log(doc);
-            res.status(200).json({
-                message: 'Handling GET requests to /products/productId',
-                doc
-            });
+            if (doc) {
+                res.status(200).json({
+                    message: 'Handling GET requests to /products/productId',
+                    doc
+                });
+            } else {
+                res.status(404).json({
+                    message: 'Invalid ID: No product exists with this ID'
+                });
+            }
         })
         .catch(error => {
             console.log(error);
@@ -57,20 +76,53 @@ router.get('/:productId', (req, res, next) => {
 
 // PATCH to /products/:productId
 router.patch('/:productId', (req, res, next) => {
+
+    // get product id from params
     const id = req.params.productId;
-    res.status(200).json({
-        message: 'Handling PATCH requests to /products/productId',
-        id: id
-    });
+    
+    // allow for updating of single values or multiple values
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    
+    // update product
+    Product.updateOne({ _id: id }, { $set: updateOps })
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            res.status(200).json({
+                message: 'Handling PATCH requests to /products/productId',
+                doc
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error });
+        });
+    
 });
 
 // DELETE to /products/:productId
 router.delete('/:productId', (req, res, next) => {
+    
+    // get product id from params
     const id = req.params.productId;
-    res.status(200).json({
-        message: 'Handling DELETE requests to /products/productId',
-        id: id
-    });
+
+    // delete product by id
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Handling DELETE requests to /products/productId',
+                result
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error });
+        });
 });
 
 // export router
